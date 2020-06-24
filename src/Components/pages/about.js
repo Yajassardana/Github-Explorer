d71bbf7eb66e98b28809b7860043f8ad3d88960a
Yajas Sardana <yajassardana@gmail.com>
2020-06-24 22:56:19 +0530

    fix about page margin

--- src/Components/pages/about.js
+++ src/Components/pages/about.js
@@ -1,14 +1,14 @@
-import React,{Fragment} from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 const About = (props) => {
-  return (
-    <Fragment>
-      <h1>About this app</h1>
-      <h4>App to search Github users</h4>
-      <p>Version 1.0.0 </p>
-    </Fragment>
-  );
-}
+	return (
+		<div style={{ marginTop: '10%' }}>
+			<h1>About this app</h1>
+			<h4>App to search Github users</h4>
+			<p>Version 1.0.0 </p>
+		</div>
+	);
+};
 
-export default About
+export default About;
